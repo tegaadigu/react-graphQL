bdf3a88031c56381cf392df43b6ebb3c81822f96
Tega Adigu <tega.adigu@gmail.com>
2018-03-11 20:02:59 -0600

    removed unused files

--- src/stateManager/reducers/index.js
+++ src/stateManager/reducers/index.js
@@ -3,10 +3,8 @@
  */
 import { combineReducers } from 'redux'
 
-// import registrationReducer from './registration.reducer';
 import contactReducer from './contact.reducer';
 
 export default combineReducers({
     contactReducer,
-    // registrationReducer
 })
